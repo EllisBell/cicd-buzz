ef798ee6499ee8a4c7698ea03ebdd8ec68f40e92
EllisBell <tee.vee.zed@gmail.com>
2020-08-02 09:32:19 +0100

    add more hype

--- buzz/generator.py
+++ buzz/generator.py
@@ -18,7 +18,7 @@ def generate_buzz():
     buzz_terms = sample(buzz, 2)
     phrase = ' '.join([sample(adjectives), buzz_terms[0], sample(adverbs),
         sample(verbs), buzz_terms[1]])
-    return phrase.title()
+    return phrase.title() + "!!!"
 
 if __name__ == "__main__":
     print(generate_buzz())
